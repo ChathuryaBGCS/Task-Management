b2ee80dec56ad7ce56a16b1a272745056a5baf39
Chathurya Nanayakkara <chathurya.nanayakkara@gmail.com>
2024-10-04 01:31:08 +0530

    Finished UI

--- app/src/main/java/com/example/taskmanagement/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagement/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.taskmanagement
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.taskmanagement.databinding.ActivityMainBinding
+import com.example.taskmanagement.utils.setupDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialog: Dialog by lazy {
+        Dialog(this,R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+
+    private val updateTaskDialog: Dialog by lazy {
+        Dialog(this,R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.update_task_dialog)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(mainBinding.root)
+
+        val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.close)
+        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.close)
+
+        addCloseImg.setOnClickListener { addTaskDialog.dismiss() }
+        updateCloseImg.setOnClickListener { updateTaskDialog.dismiss() }
+
+        mainBinding.addTaskBtn.setOnClickListener {
+            addTaskDialog.show()
         }
     }
 }
---
+++ app/src/main/java/com/example/taskmanagement/utils/util.kt
@@ -0,0 +1,13 @@
+package com.example.taskmanagement.utils
+
+import android.app.Dialog
+import android.widget.LinearLayout
+
+fun Dialog.setupDialog(layoutResId: Int) {
+    setContentView(layoutResId)
+    window!!.setLayout(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    )
+    setCancelable(false)
+}
